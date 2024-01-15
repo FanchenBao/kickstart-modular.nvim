735d71fc7a56d4908880cf27ccff29b329550aeb
Fanchen Bao <jonathonbao@gmail.com>
2024-01-14 16:45:02 -0800

    add python3 provider to speed up starting time of Python

--- lua/options.lua
+++ lua/options.lua
@@ -48,3 +48,6 @@ vim.wo.number = true
 vim.wo.relativenumber = true
 vim.opt.colorcolumn = '79,120'
 
+--[[ Python set up ]]
+-- refer to https://neovim.io/doc/user/provider.html#provider-python
+vim.g.python3_host_prog = '~/.pyenv/versions/pynvim/bin/python'
