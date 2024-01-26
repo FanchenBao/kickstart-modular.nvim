08fac9655e7c6c4a88fe67d326b40eb03d54109e
fanchen.bao <fanchen.bao@servicenow.com>
2024-01-26 01:15:35 -0800

    add key mapping to handle matching symbols such as brackets parentheses and so on

--- lua/custom/config.lua
+++ lua/custom/config.lua
@@ -1,5 +1,7 @@
+local config = {}  -- namespace
 -- [[ Custom Keymaps ]]
 local opts = { noremap = true, silent = true }
+local opts_expr = { noremap = true, silent = true, expr = true }
 vim.keymap.set("i", "jj", "<Esc>", opts)
 vim.keymap.set("t", "<C-w>h", "<C-\\><C-n><C-w>h", opts)
 -- indent by pressing > or < multiple times
@@ -11,6 +13,54 @@ vim.api.nvim_set_keymap("n", "<Leader>nc", ":lua require('neogen').generate({ ty
 vim.api.nvim_set_keymap("n", "<Leader>nt", ":lua require('neogen').generate({ type = 'type' })<CR>", opts)
 -- Map ctrl-w v to open a new buffer
 vim.keymap.set('n', '<C-w>v', '<esc>:vnew<cr>', opts)
+
+
+-- Autocomplete matching braces, brackets, parentheses, and quotations
+local matching_symbols = { '()', '{}', '[]', '<>', '""', "''" }
+
+---@param rel number relative position to the current cursor. Can be negative 
+---@return string # the character at rel position to cursor
+function config.get_char_relative_to_cursor(rel)
+  local col = vim.api.nvim_win_get_cursor(0)[2]
+  local char = vim.api.nvim_get_current_line():sub(col + rel, col + rel)
+  return char
+end
+
+--- Determine whether the cursor is in between matching symbols
+---@return boolean # true => cursor is between matching symbols, otherwise false
+function config.is_cursor_between_matching_symbols()
+  local char = config.get_char_relative_to_cursor(1)
+  local char_before = config.get_char_relative_to_cursor(0)
+  for _, symbols in ipairs(matching_symbols) do
+    if symbols == char_before .. char then
+      return true
+    end
+  end
+  return false
+end
+
+for _, symbols in ipairs(matching_symbols) do
+  -- type double matching symbols, keep the cursor in between
+  vim.keymap.set('i', symbols, symbols .. '<Esc>ha', opts)
+  -- -- type a left matching symbol, if nothing is on the right, auto fill the
+  -- -- right symbol and move the cursor in between. If there is something on
+  -- -- the right, only input the left matching symbol
+  -- vim.keymap.set('i', symbols:sub(1, 1), function()
+  --   local char = config.get_char_relative_to_cursor(1)
+  --   return char ~= '' and symbols:sub(1, 1) or symbols .. '<Esc>ha'
+  -- end, opts_expr)
+  -- when cursor is in between matching symbols and we press CR, automatically
+  -- create extra line with indent
+  vim.keymap.set('i', '<CR>', function()
+     return config.is_cursor_between_matching_symbols() and '<CR><Esc>ko' or '<CR>'
+  end, opts_expr)
+  -- when cursor is in between matching symbols and we press BS, automatically
+  -- delete both matching symbols
+  vim.keymap.set('i', '<BS>', function ()
+    return config.is_cursor_between_matching_symbols() and '<Esc>la<BS><BS>' or '<BS>'
+  end, opts_expr)
+end
+
 
 --[[ Custom LSP is configured in lsp-setup.lua ]]
 
