4920bc944dace5850d6cba5dddba2d2cb0e62f70
Fanchen Bao <jonathonbao@gmail.com>
2024-02-04 20:03:13 -0800

    remove nvim-java, set up Java LSP and debugger from scratch

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -75,9 +75,6 @@ require('mason-lspconfig').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 
--- Follow the instructions here for Java:
--- https://github.com/nvim-java/nvim-java?tab=readme-ov-file#hammer-how-to-install
-require('java').setup()
 local servers = {
   -- clangd = {},
   -- gopls = {},
