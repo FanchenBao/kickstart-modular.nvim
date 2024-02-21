2dab75b2787f6f6500e536315294dea20c6fe240
fanchen.bao <fanchen.bao@servicenow.com>
2024-02-21 00:32:40 -0800

    force prettier to use local .prettierrc.js

--- lua/custom/plugins/mason-null-ls.lua
+++ lua/custom/plugins/mason-null-ls.lua
@@ -27,7 +27,12 @@ return {
     null_ls.setup({
       sources = {
         -- null_ls.builtins.diagnostics.eslint,  -- seems to have been already set up by eslint-lsp
-        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.prettier.with {
+          condition = function (utils)
+            return utils.root_has_file {'.prettierrc.js'}
+          end,
+          prefer_local = 'node_modules/.bin',
+        },
         null_ls.builtins.diagnostics.mypy,
         null_ls.builtins.diagnostics.ruff,
         null_ls.builtins.formatting.black,
