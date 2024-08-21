319dc5debf42b2d0484f58f8b61cbeb6a2dc7e07
Fanchen Bao <jonathonbao@gmail.com>
2024-08-21 12:24:57 -0700

    fix: use a different markdown package that can disable concealing

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -1,7 +1,11 @@
 return {
-  "ixru/nvim-markdown",
+  'plasticboy/vim-markdown',
+  branch = 'master',
+  require = {'godlygeek/tabular'},
   config = function()
+    vim.g.vim_markdown_folding_disabled = 1
     vim.g.vim_markdown_conceal = 0
+    vim.g.vim_markdown_conceal_code_blocks = 0
     vim.g.vim_markdown_math = 1
     vim.g.vim_markdown_frontmatter = 1
     vim.g.vim_markdown_toml_frontmatter = 1
